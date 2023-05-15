f30cc39a36c81fb3608f8b2e8d3adbbb4362357a
Shahir Chowdhury <shahirchowdhury9800@gmail.com>
2023-05-15 16:55:07 +0000

    tests broke

--- springboot/src/test/java/ca/jrvs/apps/trading/integ/QuoteDaoIntegTest.java
+++ springboot/src/test/java/ca/jrvs/apps/trading/integ/QuoteDaoIntegTest.java
@@ -1,5 +1,8 @@
 package ca.jrvs.apps.trading.integ;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import ca.jrvs.apps.trading.TestConfig;
 import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.model.domain.Quote;
@@ -39,8 +42,21 @@ public class QuoteDaoIntegTest {
 
     @Test
     public void saveExistsAndFindById() {
-        Quote quoteToSave = new Quote();
+        Quote saveQuote = new Quote();
 
-        quoteToSave.setAskPrice(5d);
+        saveQuote.setAskPrice(5d);
+        saveQuote.setAskSize(10);
+        saveQuote.setBidPrice(15d);
+        saveQuote.setBidSize(5);
+        saveQuote.setId("FB");
+        saveQuote.setLastPrice(4d);
+        Quote savedQuote = quoteDao.save(saveQuote);
+        assertEquals(saveQuote.getId(), savedQuote.getId());
+
+        boolean existsFB = quoteDao.existsById("FB");
+        assertTrue(existsFB);
+
+        Quote findQuote = quoteDao.findById("FB").get();
+        assertEquals(findQuote.getAskPrice(), saveQuote.getAskPrice());
     }
 }
